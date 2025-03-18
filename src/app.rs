d001918d8c5e019036c7337feeafa6f6e10b9d2d
Isaac Corbrey <icorbrey@gmail.com>
2025-03-18 08:01:49 -0400

    feat: Create splash screen

---
+++ src/app.rs
@@ -0,0 +1,53 @@
+//! Application initialization.
+
+use std::time::Duration;
+
+use bevy::{app::ScheduleRunnerPlugin, prelude::*, state::app::StatesPlugin};
+use bevy_ratatui::{event::KeyEvent, RatatuiPlugins};
+use crossterm::event::KeyCode;
+
+use crate::screens;
+
+pub fn plugin(app: &mut App) {
+    app.configure_sets(
+        Update,
+        (
+            AppSet::TickTimers,
+            AppSet::RecordInput,
+            AppSet::Update,
+            AppSet::PrepFrames,
+            AppSet::Render,
+        ),
+    );
+
+    let frame_time = Duration::from_secs_f32(1.0 / 60.0);
+
+    app.add_plugins((
+        MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(frame_time)),
+        RatatuiPlugins::default(),
+        StatesPlugin,
+    ));
+
+    app.add_plugins(screens::plugin);
+
+    app.add_systems(Update, read_quit.in_set(AppSet::RecordInput));
+}
+
+/// Quits the application if the user presses Q.
+fn read_quit(mut events: EventReader<KeyEvent>, mut exit: EventWriter<AppExit>) {
+    for event in events.read() {
+        if let KeyCode::Char('q') = event.code {
+            exit.send_default();
+        }
+    }
+}
+
+/// Coordinates systems running under the [`Update`] schedule.
+#[derive(SystemSet, Debug, Clone, Copy, Eq, PartialEq, Hash, PartialOrd, Ord)]
+pub enum AppSet {
+    TickTimers,
+    RecordInput,
+    Update,
+    PrepFrames,
+    Render,
+}
