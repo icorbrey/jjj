8ee6eeb33db2197a53081ca15823e2d5ecd3db9a
Isaac Corbrey <icorbrey@gmail.com>
2025-04-11 15:21:50 -0400

    feat: Configure jjj via jj config
    This change piggybacks off of jj's configuration stack, with custom
    fields living at `[jjj]`. Right now this configuration includes the log
    polling system and the splash screen.

---
+++ src/backend/config.rs
@@ -0,0 +1,90 @@
+use anyhow::anyhow;
+use bevy::prelude::*;
+use serde::{de::DeserializeOwned, Deserialize};
+
+use super::execute_jj_command;
+
+pub fn plugin(app: &mut App) {
+    app.insert_resource(init_config());
+}
+
+#[tracing::instrument(skip_all)]
+fn init_config() -> Config {
+    let config_toml = execute_jj_command(vec!["config", "list"])
+        .map_err(|e| anyhow!("Failed to read config: {e}"))
+        .unwrap();
+
+    let root = toml::de::from_str::<ConfigRoot>(&config_toml)
+        .map_err(|e| anyhow!("Failed to parse config: {e}"))
+        .unwrap();
+
+    info!("{:?}", &root.jjj);
+
+    root.jjj
+}
+
+#[allow(unused)]
+#[derive(Debug, Deserialize)]
+struct ConfigRoot {
+    #[serde(default = "default_table")]
+    jjj: Config,
+}
+
+fn default_table<De: DeserializeOwned>() -> De {
+    toml::de::from_str::<De>("").unwrap()
+}
+
+/// Application configuration.
+#[derive(Debug, Deserialize, Reflect, Resource)]
+#[serde(rename_all = "kebab-case")]
+pub struct Config {
+    /// Logging configuration.
+    #[serde(default = "default_table")]
+    pub log: LogConfig,
+
+    /// Splash screen configuration.
+    #[serde(default = "default_table")]
+    pub splash: SplashConfig,
+}
+
+/// Configuration for logging.
+#[derive(Debug, Deserialize, Reflect)]
+#[serde(rename_all = "kebab-case")]
+pub struct LogConfig {
+    /// How frequently jjj should check the status of the current repo.
+    #[serde(default = "LogConfig::default_poll_interval_ms")]
+    pub poll_interval_ms: u64,
+}
+
+impl LogConfig {
+    fn default_poll_interval_ms() -> u64 {
+        1000
+    }
+}
+
+/// Configuration for the splash screen.
+#[derive(Debug, Deserialize, Reflect)]
+#[serde(rename_all = "kebab-case")]
+pub struct SplashConfig {
+    /// Whether to skip the splash screen on startup.
+    #[serde(default)]
+    pub skip: bool,
+
+    /// The total max duration the splash screen should be displayed for.
+    #[serde(default = "SplashConfig::default_total_duration_ms")]
+    pub total_duration_ms: u64,
+
+    /// The duration between splash screen animation frames.
+    #[serde(default = "SplashConfig::default_line_interval_ms")]
+    pub line_interval_ms: u64,
+}
+
+impl SplashConfig {
+    fn default_total_duration_ms() -> u64 {
+        1950
+    }
+
+    fn default_line_interval_ms() -> u64 {
+        150
+    }
+}
--- src/screens/mod.rs
+++ src/screens/mod.rs
@@ -2,18 +2,30 @@
 
 use bevy::prelude::*;
 
+use crate::backend::config::Config;
+
 pub mod interface;
 pub mod splash;
 
 /// Sets up the application screens.
 #[tracing::instrument(skip_all)]
 pub fn plugin(app: &mut App) {
-    app.init_state::<Screen>();
     app.enable_state_scoped_entities::<Screen>();
+    app.init_state::<Screen>();
+
+    app.add_systems(Startup, init_state);
 
     app.add_plugins((splash::plugin, interface::plugin));
 
     debug!("Finished loading");
+}
+
+fn init_state(mut next_state: ResMut<NextState<Screen>>, config: Res<Config>) {
+    next_state.set(if !config.splash.skip {
+        Screen::Splash
+    } else {
+        Screen::Interface
+    });
 }
 
 /// Determines what screen should be shown.
