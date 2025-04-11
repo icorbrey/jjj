d54e33d82ccd3e070699862e342dbe755a018af9
Isaac Corbrey <icorbrey@gmail.com>
2025-04-11 12:35:27 -0400

    feat: Add global logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#![feature(panic_update_hook)]
+
 pub mod app;
 pub mod backend;
 pub mod cli;
 pub mod errors;
 pub mod events;
 pub mod frontend;
+pub mod logger;
 pub mod screens;
 pub mod utils;
 
@@ -12,6 +15,10 @@ use clap::Parser;
 use cli::Cli;
 
 fn main() {
+    logger::install();
+
     Cli::parse();
     App::new().add_plugins(app::plugin).run();
+
+    logger::dump();
 }
